10b7286a5e65e492b8ea4c178c0967145f452a05
Manoj Prabahar Raju <manojm95@gmail.com>
2019-09-19 00:51:41 -0400

    test

--- index-db.js
+++ index-db.js
@@ -8,6 +8,7 @@ var con = mysql.createConnection({ host: process.env.MYSQL_HOST, user: process.e
 con.connect(function(err){
   if(err){
     console.log('Error connecting to db: ', err);
+    console.log('Error connecting to db: ', err);
     return;
   }
   console.log('Connection to db established');
