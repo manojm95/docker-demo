2e55ee4d12a428ea84f46497048321e35ff6d018
Manoj Prabahar Raju <manojm95@gmail.com>
2019-09-19 00:31:34 -0400

    test

--- index-db.js
+++ index-db.js
@@ -3,10 +3,11 @@ var app = express();
 var mysql = require("mysql");
 var con = mysql.createConnection({ host: process.env.MYSQL_HOST, user: process.env.MYSQL_USER, password: process.env.MYSQL_PASSWORD, database: process.env.MYSQL_DATABASE});
 
-// mysql code
+// mysql code code
 
 con.connect(function(err){
   if(err){
+    console.log('Error connecting to db: ', err);
     console.log('Error connecting to db: ', err);
     return;
   }
